47cef6082e8844b61c08d90d0a7b575448103d15
rjeth <40770876+rjeth@users.noreply.github.com>
2018-12-07 16:49:31 +0800

    profile

--- components/profile.php
+++ components/profile.php
@@ -3,16 +3,19 @@
     <div class="row">
       <div class="col-lg-3 mb-4">
         <div class="list-group z-depth-1">
-          <a href="#" class="list-group-item active">
-          Profile</a>
-          <a href="#" class="list-group-item list-group-item-action">Account</a>
+          <a href="#panel104" class="list-group-item active" data-toggle="tab" role="tab">
+            Profile</a>
+            <a href="#panel105" class="list-group-item" data-toggle="tab" role="tab">
+              Account</a>
+
 
         </div>
       </div>
       <div class="col-lg-6 mb-4">
 
-        <div class="card">
-          <div class="card-body">
+        <div class="tab-content card">
+          <!--Panel 1-->
+          <div class="tab-pane fade in show active" id="panel104" role="tabpanel">
             <div class="text-left">
               <h2>Profile</h2>
               <hr>
@@ -21,26 +24,90 @@
             <div class="w-50">
               <div class="md-form">
                 <label for="surname" class="active">Account Id</label>
-                <input type="text" name="acro" class="form-control" id="acro" required="required" value="sampple" disabled>
+                <p>sampple</p>
+              </div>
+
+              <div class="md-form mt-2">
+                <label for="surname" class="active">Username</label>
+                <p>sampple</p>
+              </div>
+
+              <div class="md-form mt-2">
+                <label for="surname" class="active">Date Created</label>
+                <p>2018-12-07 16:33:47</p>
+              </div>
+            </div>
+          </div>
+          <!--/.Panel 1-->
+          <!--Panel 2-->
+          <div class="tab-pane fade" id="panel105" role="tabpanel">
+            <div class="text-left">
+              <h2>Account</h2>
+              <hr>
+            </div>
+
+            <div class="w-50">
+              <h4>Change Password</h4>
+              <div class="md-form">
+                <label for="surname" class="active">Current password</label>
+                <input type="text" name="acro" class="form-control" id="acro" required="required" placeholder=" ">
                 <div class="invalid-feedback">
                   Acronym is required
                 </div>
               </div>
 
-              <div class="md-form mt-5">
-                <label for="surname" class="active">Username</label>
-                <input type="text" name="acro" class="form-control" id="acro" required="required" value="sampple" disabled>
+              <div class="md-form">
+                <label for="surname">new password</label>
+                <input type="text" name="acro" class="form-control" id="acro" required="required" placeholder=" ">
                 <div class="invalid-feedback">
                   Acronym is required
                 </div>
               </div>
+
+              <div class="md-form">
+                <label for="surname">confirm new password</label>
+                <input type="text" name="acro" class="form-control" id="acro" required="required" placeholder=" ">
+                <div class="invalid-feedback">
+                  Acronym is required
+                </div>
+              </div>
+
+
+              <div class="mt-2">
+                <button class="btn aqua-gradient btn-rounded waves-effect waves-light z-depth-0" type="submit">Update password</button>
+              </div>
+
             </div>
 
 
+            <div class="w-50 mt-5">
+              <h4>Change username</h4>
+              <div class="md-form">
+                <label for="surname">new username</label>
+                <input type="text" name="acro" class="form-control" id="acro" required="required" placeholder=" ">
+                <div class="invalid-feedback">
+                  Acronym is required
+                </div>
+              </div>
+
+
+              <div class="mt-2">
+                <button class="btn aqua-gradient btn-rounded waves-effect waves-light z-depth-0" type="submit">Update Username</button>
+              </div>
+
+            </div>
           </div>
+          <!--/.Panel 2-->
+          <!--Panel 3-->
+          <div class="tab-pane fade" id="panel106" role="tabpanel">
+            <p>Lorem ipsum dolor sit amet, consectetur adipisicing elit. Nihil odit magnam minima, soluta doloribus reiciendis molestiae placeat unde eos molestias. Quisquam aperiam, pariatur. Tempora, placeat ratione porro voluptate odit minima.</p>
+          </div>
+          <!--/.Panel 3-->
         </div>
+
       </div>
+
     </div>
-  </section>
+</section>
 
 </section>
