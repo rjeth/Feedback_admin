1ab3b0ed0e8b5a1d46c3142503516b0922f28e84
rjeth <40770876+rjeth@users.noreply.github.com>
2018-12-08 22:24:52 +0800

    dashboard

--- components/dashboard.php
+++ components/dashboard.php
@@ -73,23 +73,5 @@
       </div>
     </div>
 
-    <div class="col-xl-3 col-md-6 mb-4">
-      <div class="card card-cascade cascading-admin-card">
-        <div class="admin-up">
-          <i class="fa fa-bar-chart red accent-2"></i>
-          <div class="data">
-            <p>ORGANIC TRAFFIC</p>
-            <h4 class="font-weight-bold dark-grey-text">2000</h4>
-          </div>
-        </div>
-        <div class="card-body card-body-cascade">
-          <div class="progress mb-3">
-            <div class="progress-bar bg-primary" role="progressbar" style="width: 25%" aria-valuenow="25" aria-valuemin="0" aria-valuemax="100"></div>
-          </div>
-          <p class="card-text">Better than last week (25%)</p>
-        </div>
-      </div>
-    </div>
-
   </div>
 </section>
